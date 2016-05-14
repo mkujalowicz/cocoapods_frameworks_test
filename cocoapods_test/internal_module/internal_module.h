ce8964b819948d1734b600aa17b4d3538151e8a7
Maciej Kujalowicz <maciej@kujalowicz.com>
2016-05-14 16:50:51 -0700

    add internal module with tests

---
+++ cocoapods_test/internal_module/internal_module.h
@@ -0,0 +1,19 @@
+//
+//  internal_module.h
+//  internal_module
+//
+//  Created by mac on 5/14/16.
+//  Copyright © 2016 Maciej Kujalowicz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for internal_module.
+FOUNDATION_EXPORT double internal_moduleVersionNumber;
+
+//! Project version string for internal_module.
+FOUNDATION_EXPORT const unsigned char internal_moduleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <internal_module/PublicHeader.h>
+
+
