74c572e8c169381822b0ebbca18130fa971caa83
Maciej Kujalowicz <maciej@kujalowicz.com>
2016-05-14 16:45:26 -0700

    add initial project

---
+++ cocoapods_test/cocoapods_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cocoapods_test
+//
+//  Created by mac on 5/14/16.
+//  Copyright © 2016 Maciej Kujalowicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
