0276b3e277cfc5edc49ca6a6da4212c8fb641d85
Maciej Kujalowicz <maciej@kujalowicz.com>
2016-05-14 17:26:26 -0700

    add custom class which inherits from a pod

---
+++ cocoapods_test/internal_module/CustomBEMSimpleLineGraphView.swift
@@ -0,0 +1,15 @@
+//
+//  CustomBEMSimpleLineGraphView.swift
+//  cocoapods_test
+//
+//  Created by mac on 5/14/16.
+//  Copyright © 2016 Maciej Kujalowicz. All rights reserved.
+//
+
+import Foundation
+import BEMSimpleLineGraphView
+
+@IBDesignable public class CustomBEMSimpleLineGraphView: BEMSimpleLineGraphView {
+    
+}
+
